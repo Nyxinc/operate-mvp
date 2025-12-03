d0f7bf34a07a7af972ec39bfa81b10edee2713dd
Nyxinc <zeinhamade01@gmail.com>
2025-12-03 14:04:22 +0200

    fix: install auth dependencies and create dashboard redirect page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col items-center justify-center p-8">
+      <h1 className="text-4xl font-extrabold text-green-600">
+        Welcome to the OPERATE Dashboard!
+      </h1>
+      <p className="text-xl text-gray-700 mt-4">
+        You are successfully logged in. This is the next page we will build.
+      </p>
+    </div>
+  );
+}
